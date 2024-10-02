318a33cd455cab1c3b5badec5c9866d9468eb009
apatni123 <aishah.patni@barclays.com>
2024-10-02 16:32:23 +0100

    all done

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,26 @@
 // import the Media class:
-
+const Media = require('./Media')
 // create your Music class:
-
+class Music extends Media{
+    constructor(title,year,genre,artist,length){
+        super(title,year,genre)
+        this.artist=artist
+        this.length=length       
+    }
+    summary(){
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
+    }
+    static shortestAlbum(music) {
+        
+        let shortestAlbum = music[0];
+        for (let i = 1; i < music.length; i++) {
+            if (music[i].length < shortestAlbum.length) {
+                shortestAlbum = music[i]; 
+            }
+        }
+    
+        return shortestAlbum; // Return the book with the highest rating
+    }
+}
 // don't change below
 module.exports = Music;
